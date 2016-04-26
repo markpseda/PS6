3d6bcdfed6df8d7743ccd731275677fe47e95e72
Mark Seda <markpseda@gmail.com>
2016-04-25 21:38:27 -0400

    first

---
+++ PS6_DAL/src/main/java/domain/PersonDomainModel.java
@@ -0,0 +1,112 @@
+package domain;
+
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import util.LocalDateAdapter;
+
+public class PersonDomainModel {
+
+    private  String firstName;
+    private  String lastName;
+    private  String street;
+    private  Integer postalCode;
+    private  String city;
+    private  LocalDate birthday;
+    private  UUID personID;
+
+    /**
+     * Default constructor.
+     */
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String firstNameProperty() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String lastNameProperty() {
+        return lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String streetProperty() {
+        return street;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public int postalCodeProperty() {
+        return postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String cityProperty() {
+        return city;
+    }
+
+    @XmlJavaTypeAdapter(LocalDateAdapter.class)
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public LocalDate birthdayProperty() {
+        return birthday;
+    }
+    
+    public void setPersonID() {
+    	this.personID = UUID.randomUUID();
+    }
+    public UUID getPersonID() {
+    	return this.personID;
+    }
+}
---
+++ PS6_DAL/src/test/java/base/Student_Test.java
@@ -0,0 +1,61 @@
+package base;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import domain.PersonDomainModel;
+import base.PersonDAL;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.StringProperty;
+
+public class Student_Test {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void Addtest() {
+		PersonDomainModel testPerson = new PersonDomainModel();
+		testPerson.setFirstName("Mark");
+		testPerson.setLastName("Seda");
+		testPerson.setCity("Dover");
+		testPerson.setPostalCode(12344);
+		testPerson.setStreet("251 Mifflin Road");
+		testPerson.setPersonID();
+		PersonDAL.addPerson(testPerson);
+	}
+	@Test
+	public void Deletetest() {
+		assertTrue(1==1);
+	}
+	@Test
+	public void Updatetest() {
+		assertTrue(1==1);
+	}
+	@Test
+	public void Gettest() {
+		assertTrue(1==1);
+	}
+
+}
